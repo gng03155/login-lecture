12e07641000289969eb059001525af25d79fd005
jongseon <jongseon@test.com>
2021-04-27 17:51:24 +0900

    dom 이용한 html객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,20 @@
 "use strict";
 
-console.log("dd");
-console.log("nodemon");
-console.log("nodemon2")
+const id = document.querySelector("#id");
+const pw = document.querySelector("#password");
+const loginBtn = document.querySelector("#login");
+
+
+const login = () => {
+    const req = {
+        id: id.value,
+        pw: pw.value,
+    }
+
+    console.log(req);
+
+
+};
+
+
+loginBtn.addEventListener("click", login);
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -1,7 +1,10 @@
 "use strict";
 
 const express = require("express");
+
+//이렇게 선언하면 app에서 세팅한 값을 받아올 수 없음
 // const router = express();
+
 const router = express.Router();
 
 const { hello, login } = require("./home.ctrl");
@@ -12,3 +15,4 @@ router.get("/", hello);
 router.get("/login", login);
 
 module.exports = router;
+
